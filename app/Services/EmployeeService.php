66ec48cd747e140226aecc203183bbd445de94a6
hari <harirs@gmail.com>
2024-04-15 06:48:58 +0530

    empl sync

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -10,6 +10,7 @@ use App\Models\EmployeeToSeat;
 use App\Models\Punching;
 use App\Models\Section;
 use App\Models\EmployeeToSection;
+use App\Models\EmployeeToDesignation;
 
 class EmployeeService {
 
@@ -41,6 +42,142 @@ class EmployeeService {
     {
         $aebas_employees = (new AebasFetchService())->fetchApi(1,0);
         //\Log::info('got emp' . count($aebas_employees));
+
+        $aebas_desig_to_ourId = array(
+            "Additional Personal Assistant to Deputy Speaker" => 1,
+            "Additional Personal Assistant to Speaker" => 2,
+            "Additional Private Secretary to Deputy Speaker" => 3,
+            "additional private Secretary to Speaker" => 4,
+            "Additional Secretary" => 5,
+            "Amenities Assistant" => 6,
+            "Amenities Supervisor" => 7,
+            "Assembly Attendant" => 8,
+            "Assistant" => 9,
+            "Assistant (Office of the Deputy Speaker)" => 10,
+            "Assistant (Office of the Speaker)" => 11,
+            "Assistant Agriculture Officer" => 12,
+            "Assistant Librarian Grade I" => 13,
+            "Assistant Librarian Grade II" => 14,
+            "Assistant Private Secretary" => 15,
+            "Assistant Private Secretary to Deputy Speaker" => 16,
+            "Assistant Section Officer" => 17,
+            "ASSISTANT SENIOR GRADE" => 18,
+            "ATTENDER GR-II" => 19,
+            "Attender(Hg)" => 20,
+            "Binder Gr I" => 21,
+            "Binder Gr II" => 22,
+            "Catalogue Assistant" => 23,
+            "Chauffeur (Office of the Deputy Speaker)" => 24,
+            "Chauffeur (Office of the Speaker)" => 25,
+            "Chief Amenities Assistant" => 26,
+            "Chief Editor" => 27,
+            "Chief Information Assistant" => 28,
+            "Chief Librarian" => 29,
+            "CHM Technician" => 30,
+            "Clerical Assistant (Grade II)" => 31,
+            "Clerical Assistant (GradeI)" => 32,
+            "Clerk ( Office of the Deputy Speaker)" => 33,
+            "Computer Assistant (Grade I)" => 34,
+            "Computer Assistant (Grade II)" => 35,
+            "Computer Assistant (Office of the Deputy Speaker)" => 36,
+            "Computer Assistant (Sel Grade)" => 37,
+            "Computer Assistant (Sen Grade)" => 38,
+            "Computer Operator cum Web Designer" => 39,
+            "Computer Programmer ( Hardware)" => 40,
+            "Computer Programmer ( Software)" => 41,
+            "Confidential Assistant  (Grade I)" => 42,
+            "Confidential Assistant  (Grade II)" => 43,
+            "Confidential Assistant (Office of the Deputy Speaker)" => 44,
+            "Confidential Assistant (Selection Grade )" => 45,
+            "Confidential Assistant (Sen Grade)" => 46,
+            "Consultant IT" => 47,
+            "COPY HOLDER" => 48,
+            "Deputy Chief Editor" => 49,
+            "Deputy Librarian" => 50,
+            "Deputy Secretary" => 51,
+            "Driver Gr I" => 52,
+            "Driver Gr II" => 53,
+            "Driver Snr Grade" => 54,
+            "DTP OPERATOR" => 55,
+            "Duffedar" => 56,
+            "Editor of Debates" => 57,
+            "Foreman" => 58,
+            "Full Time Cleaner" => 59,
+            "Full Time Gardner" => 60,
+            "Full Time Sanitation Worker" => 61,
+            "Full Time Sweeper" => 62,
+            "Head Driver" => 63,
+            "Head Gardener" => 64,
+            "Helper" => 65,
+            "Information Assistant" => 66,
+            "Joint Chief Editor" => 67,
+            "Joint Secretary" => 68,
+            "Junior Health Inspector" => 69,
+            "Lady Attendant" => 70,
+            "Librarian" => 71,
+            "Lift Operator" => 72,
+            "Lift Supervisor" => 73,
+            "Mochee" => 74,
+            "NETWORK ADMINISTRATOR(NG)" => 75,
+            "Office Attendant" => 76,
+            "Office Attendant  GrI" => 77,
+            "Office Attendant (Office of the Deputy Speaker)" => 78,
+            "Office Attendant (Office of the Speaker)" => 79,
+            "Office Superintendant" => 80,
+            "Office Superintendent (Office of Speaker)" => 81,
+            "Offset Machine Operator" => 82,
+            "Part time Cleaner" => 83,
+            "Part Time Gardener" => 84,
+            "Part Time Sanitary Worker" => 85,
+            "Part Time Sweeper" => 86,
+            "Part Time Sweeper Cum Sanitation Worker" => 87,
+            "PASTE-UP ARTIST GRADE II" => 88,
+            "Personal Assistant" => 89,
+            "Personal Assistant to Speaker" => 90,
+            "PERSONAL SECRETARY" => 91,
+            "Photocopier Operator" => 92,
+            "Press Secretary to Speaker" => 93,
+            "Private Secretary to Speaker" => 94,
+            "Pump Operator" => 95,
+            "READER GRADE II" => 96,
+            "Reporter Grade I" => 97,
+            "Reporter Grade II" => 98,
+            "Roneo  Operator" => 99,
+            "Secretary" => 100,
+            "Section Officer" => 101,
+            "Selection Grade Reporter" => 102,
+            "Senior Grade Reporter" => 103,
+            "Special Secretary" => 104,
+            "System Administrator (Software)" => 105,
+            "System Administrator (Hardware)" => 106,
+            "Under Secretary" => 107,
+            "Video Editor" => 108
+        );
+
+
+        foreach ($aebas_employees as $key => $emp) {
+
+
+            if(!Employee::where('aadhaarid',$emp[0])->first()){
+               $id = Employee::create([
+                    'aadhaarid' => $emp[0],
+                    'name' => $emp[12],
+                ])->id;
+
+                EmployeeToDesignation::create([
+
+                    'employee_id' =>  $id,
+                    'designation_id' => $aebas_desig_to_ourId[  $emp[11] ],
+                    'start_date' =>  '2024-01-01',
+
+                ]
+                );
+            }
+
+        }
+
+
+
         return $aebas_employees;
     }
 
@@ -74,7 +211,7 @@ class EmployeeService {
         return $employee_section_maps->count() ? $employee_section_maps : null;
     }
 
-    public function getLoggedUserSubordinateEmployees()
+    public function getLoggedUserSubordinateEmployees($date)
     {
           //get current logged in user's charges
           $me = User::with('employee')->find(auth()->id());
