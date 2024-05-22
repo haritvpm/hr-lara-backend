b2cdfca539ab02cbc8ad64d9124a32584a7fb22c
hari <hari.rs@gmail.com>
2024-05-22 12:20:42 +0530

    punch fix

--- resources/views/admin/employeeToFlexis/index.blade.php
+++ resources/views/admin/employeeToFlexis/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-@can('employee_to_flexi_create')
+@can('employee_to_section_create')
     <div style="margin-bottom: 10px;" class="row">
         <div class="col-lg-12">
             <a class="btn btn-success" href="{{ route('admin.employee-to-flexis.create') }}">
